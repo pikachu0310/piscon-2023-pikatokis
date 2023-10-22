84bc6ee3de1a67aa3aa953179f6ee521a68acfca
pikachu0310 <sunnon@i.softbank.jp>
2023-10-22 14:49:33 +0900

    add apply-config.sh

---
+++ apply-config.sh
@@ -0,0 +1,3 @@
+sudo cp s1/etc/mysql/mysql.conf.d /etc/mysql/mysql.conf.d/mysqld.cnf
+sudo cp s1/etc/nginx /etc/nginx/nginx.conf
+sudo cp s1/etc/nginx/sites-available /etc/nginx/sites-available/isucari.conf
