3c9d15b73a5bf771816794f9ebb9c3c8c168f438
pikachu0310 <sunnon@i.softbank.jp>
2023-10-22 16:13:08 +0900

    .sh to Taskfile.yaml

--- apply-config.sh
+++
@@ -1,9 +0,0 @@
-set -eux
-
-sudo cp s1/etc/mysql/mysql.conf.d/mysqld.cnf /etc/mysql/mysql.conf.d/mysqld.cnf
-sudo cp s1/etc/nginx/nginx.conf /etc/nginx/nginx.conf
-sudo cp s1/etc/nginx/sites-available/isucari.conf /etc/nginx/sites-available/isucari.conf
-
-sudo nginx -t
-sudo systemctl reload nginx
-sudo systemctl restart mysql
--- before-bench.sh
+++
@@ -1,6 +0,0 @@
-set -eux
-
-cd ~/isucari && git pull
-cd ~/isucari/webapp/go && make isucari && sudo systemctl restart isucari.golang.service
-sudo pt-query-digest --filter 'length($$event->{arg}) <= 4000' /var/log/mysql/mysql-slow.log > ~/isucari/log/$(date +mysql-slow.log-%m-%d-%H-%M -d "+9 hours") && sudo rm /var/log/mysql/mysql-slow.log && sudo systemctl restart mysql
-sudo cat /var/log/nginx/access.log | alp ltsv -m "/users/\d+.json","/items/\d+.json","/new_items/\d+.json","/upload/.+.jpg","/transactions/\d+.png" --sort sum -r > ~/isucari/log/$(date +access.log-%m-%d-%H-%M -d "+9 hours") && sudo rm /var/log/nginx/access.log && sudo nginx -t && sudo systemctl reload nginx
